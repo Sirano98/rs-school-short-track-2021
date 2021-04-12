18566d20117d2726d858ee94c00e92ffb56c9382
Sirano98 <protsiuk1998@gmail.com>
2021-04-13 01:32:07 +0300

    feat: add encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let i;
+  let counter = 1;
+  let result = '';
+  const size = str.length;
+  for (i = 0; i < size; i++) {
+    if (str[i] === str[i + 1]) {
+      counter++;
+    } else if (counter === 1) {
+      result += str[i];
+      counter = 1;
+    } else {
+      result += counter + str[i];
+      counter = 1;
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
